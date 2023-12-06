e56c1c17e2bf979343aad0480a6bf7df11f8bd86
Farhad rasouli <rasouli.farhad@gmail.com>
2023-12-06 07:13:44 +0330

    updated

---
+++ src/app/root-store/index.ts
@@ -0,0 +1,6 @@
+import { RootStoreModule } from './root-store.module';
+import * as RootStoreSelectors from './selectors';
+import * as RootStoreState from './root-state';
+export * from './my-feature-store';
+export * from './user-store';
+export { RootStoreState, RootStoreSelectors, RootStoreModule };
